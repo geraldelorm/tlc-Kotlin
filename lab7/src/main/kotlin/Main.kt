39b60f4cbdfb8a8a8a4571a0f815132c36b25b41
geraldelorm <geraldelorm14@gmail.com>
2021-10-15 13:03:41 +0000

    lab7 done

---
+++ lab7/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import java.util.Random as jRan
+import kotlin.random.Random as kRan
+
+
+fun main(args: Array<String>) {
+    var jScore: Int = 0
+    var kScore: Int = 0
+    for (i in 1..1000){
+        val javaRan : Int = jRan().nextInt()
+        val kotlinRan : Int = kRan.nextInt()
+
+        if (javaRan > kotlinRan){
+            jScore++
+        } else{
+            kScore++
+        }
+    }
+
+    println("Java Random was higher $jScore times")
+    println("Kotlin Random was higher $kScore times")
+}
