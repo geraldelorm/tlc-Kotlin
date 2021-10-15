3d054d482deaab4aae99977d34041b1ae2bb9ca7
geraldelorm <geraldelorm14@gmail.com>
2021-10-15 13:31:36 +0000

    lab8 started

---
+++ lab8/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    //Q1
+    tailrec fun factorial(n : Int) : Int = if (n<0) 0 else when(n) {
+        0, 1 -> 1
+        else -> n * factorial(n -1)
+    }
+
+    var fac5 = factorial(5)
+    println(fac5)
+
+    println("------------")
+
+    //Q2
+    val doubleNum : (Int) -> Int = { i : Int -> i*2}
+    //type is int to int
+    println(doubleNum(50))
+
+    //Q3
+}
